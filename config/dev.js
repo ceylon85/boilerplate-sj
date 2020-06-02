f1ab1eb3dbb3e649ac09d32729a327fb976c3f73
ceylon85 <canleez123@gmail.com>
2020-06-02 18:09:11 +0900

    config

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI:'mongodb+srv:sj_lee:mongo1234@cluster0-mupkj.mongodb.net/test?retryWrites=true&w=majority'
-}
+    mongoURI:
+    "mongodb://sj_lee:tmdwns85@cluster0-shard-00-00-mupkj.mongodb.net:27017,cluster0-shard-00-01-mupkj.mongodb.net:27017,cluster0-shard-00-02-mupkj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+};
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
+
+const config = require("./config/key");
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -10,20 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //application.json
 app.use(bodyParser.json());
-
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb://sj_lee:mongo1234@cluster0-shard-00-00-mupkj.mongodb.net:27017,cluster0-shard-00-01-mupkj.mongodb.net:27017,cluster0-shard-00-02-mupkj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
-  .then(() => console.log("MongoDB 연결중..."))
-  .catch((err) => console.log(err));
+  .connect(config.mongoURI, {   
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true, 
+    useFindAndModify: false })
+  .then(() => console.log("mongoDB connected"))
+  .catch(err => console.error(err));
 
 app.get("/", (req, res) => res.send("Hello world이다!! Ha"));
 
