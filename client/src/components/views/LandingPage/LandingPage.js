f289b5e7a8f28a3e7590aee140578300c2f76486
ceylon85 <canleez123@gmail.com>
2020-06-06 17:25:07 +0900

    .

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import Axios from 'axios';
+import { response } from 'express';
+
 
 function LandingPage() {
+
+    useEffect(() => {
+        Axios.get('/api/hello')
+        .then(response => console.log(response.data))
+    }, [])
     return (
         <div>
             landing
