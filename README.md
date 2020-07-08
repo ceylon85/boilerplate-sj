0a61816db40c447244d35cb097b54de1884c67fc
ceylon85 <canleez123@gmail.com>
2020-07-08 15:13:16 +0900

    add readme

--- README.md
+++ README.md
@@ -3,3 +3,25 @@ React boiler-plate...
 
 `Boilerplate` 란?
 - 변경 없이 계속 재사용할 수 있는 코드
+# 
+> 개선해야 할 부분   
+- [x] 시작 페이지에 comment, like, unlike 추가
+- [ ] 아이다 & 비밀번호 찾기 기능 추가
+- [ ] 구체적인 페이지 구상 
+- [ ] 추가한 코드 리액트에 맞춰 정리 
+# 
+
+# My project List   
+## Vanila JavaScript application   
+Link to [Momentum](https://ceylon85.github.io/Todolist/)
+
+Link to [PaintJs](https://ceylon85.github.io/paintjs/)
+
+Link to [YoutubeClone](https://ceylon85.github.io/youtube-clone)   
+
+## React Application   
+Link to [DarkModeToggle](https://ceylon85.github.io/Darkmode-Toggle)   
+
+Link to [BurgerMenu](https://ceylon85.github.io/react-burger-menu)
+
+Link to [DarkThemeApp](https://ceylon85.github.io/React-theme-app/)
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,40 @@
 import React from "react";
+import Comments from "./Sections/Comments";
+import LikeDislikes from "./Sections/LikeDislikes";
 import { Typography } from "antd";
-
 
 function LandingPage() {
   const Title = Typography;
 
   return (
     <div className="app">
-      <hr/>
-      <Title style={{fontSize:'3rem'}}>시작페이지</Title>
+      <hr />
+      <Title style={{ fontSize: "3rem" }}>시작페이지</Title>
+      <div style={{ width: '85%', margin: '3rem auto' }}>
+            <h2> 테이블 </h2>
+            <hr />
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+
+                   바디
+
+
+                </tbody>
+            </table>
+        </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <LikeDislikes/>
+      </div>
+      <Comments />
     </div>
   );
 }
